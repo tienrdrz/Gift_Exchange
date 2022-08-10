ff7e8b63f2d5bc1e9137fd666268ed3ad58401bc
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-08-10 19:05:58 -0400

    item routes, item model, index.js

--- controllers/index.js
+++ controllers/index.js
@@ -7,5 +7,10 @@ const apiRoutes = require('./api');
 // Route links
 router.use('/api', apiRoutes);
 
+// Item route
+const itemRoutes = require('./item-routes');
+
+router.use('/item', itemRoutes);
+
 // Exports
 module.exports = router;
--- models/Item.js
+++ models/Item.js
@@ -2,13 +2,41 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 // Item Model
-class Item extends Model {};
+class Item extends Model { };
 
 // Item Model Initializer
 Item.init(
     // Columns
     {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
 
+        item_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
     },
     // Model Properties
     {
