3d020300dc02f05cfa56ab14213fb0eb8e5bbc13
jcg <jcg.cristel@gmail.com>
2022-08-14 00:51:27 -0400

    installation of express handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 
 // Route imports
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes')
 
 // Route links
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 // Exports
 module.exports = router;
