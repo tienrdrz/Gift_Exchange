8cbd77dfcb758b96277a69881f970af51acbf476
etienne <etiennerodriguez3@gmail.com>
2022-08-15 13:28:06 -0400

    can get individual data entries from the url (image, url, title...)

--- controllers/api/metascraper-routes.js
+++ controllers/api/metascraper-routes.js
@@ -6,10 +6,60 @@ const { route } = require('./user-routes');
 const url = 'https://www.amazon.ca/dp/B00F2C31AM/';
 
 
-//GET all users (maybe but included for now)
+//GET all meta data 
 router.get('/', (req, res) => {
     getMetaData(req.body.url)
         .then(metaData => res.json(metaData))
+        .catch(err => {
+            console.log(err);
+            res.status(500), res.json(err);
+        });
+});
+
+//GET the title of meta data
+router.get('/title', (req, res) => {
+    getMetaData(req.body.url)
+        .then(metaData => res.json(metaData.title))
+        .catch(err => {
+            console.log(err);
+            res.status(500), res.json(err);
+        });
+});
+
+//GET the description of meta data
+router.get('/desc', (req, res) => {
+    getMetaData(req.body.url)
+        .then(metaData => res.json(metaData.description))
+        .catch(err => {
+            console.log(err);
+            res.status(500), res.json(err);
+        });
+});
+
+//GET the url of meta data
+router.get('/url', (req, res) => {
+    getMetaData(req.body.url)
+        .then(metaData => res.json(metaData.url))
+        .catch(err => {
+            console.log(err);
+            res.status(500), res.json(err);
+        });
+});
+
+//GET the image of meta data
+router.get('/image', (req, res) => {
+    getMetaData(req.body.url)
+        .then(metaData => res.json(metaData.image))
+        .catch(err => {
+            console.log(err);
+            res.status(500), res.json(err);
+        });
+});
+
+//GET the icon of meta data
+router.get('/icon', (req, res) => {
+    getMetaData(req.body.url)
+        .then(metaData => res.json(metaData.icon))
         .catch(err => {
             console.log(err);
             res.status(500), res.json(err);
