fb20c39b7c75cea86c39abe3bcfcdf42538de35e
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-08-16 18:06:24 -0400

    committing before going to another feature to look at some code

--- public/javascripts/item-handlers.js
+++ public/javascripts/item-handlers.js
@@ -1,5 +1,22 @@
 // Function to handle deleting of items
-// CODE HERE
+async function deleteItem(event) {
+    // Get the item's id by targetting the item_id
+    const itemId = event.target.getAttribute('item_id');
+
+    const deleteItemRes = await fetch(`api/exchanges/${itemId}`, {
+        method: 'DELETE'
+    })
+
+    // If delete success, refresh page to reflect changes
+    if (deleteItemRes.ok) {
+        document.location.reload();
+    }
+    // Else throw an error
+    else {
+        alert('Failed to create a new exchange');
+        console.log(deleteExchangeRes.statusText);
+    }  
+}
 
 // Function to add an item to a list
 // CODE HERE
@@ -11,3 +28,4 @@
 
 // An event listener for the adding an item
 // CODE HERE
+
